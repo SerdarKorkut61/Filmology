b0496f77fd1ac4f9bdb74e303cb342c8a7e5d41a
Serdar Korkut <dkrose6160@gmail.com>
2024-12-29 17:37:26 +0300

    syfa düzeni, anasayfa tasarımı ve navbar ksımı

---
+++ resources/views/layouts/front/navbar.blade.php
@@ -0,0 +1,23 @@
+<div class="row">
+    <div class="col-md-12 bg-dark p-2">
+        <div class="row">
+            <div class="col-md-2 pl-5">
+                <img src="{{asset('image/Filmology (Transparent).png')}}" width="143px" height="79px" alt="logo png">
+            </div>
+            <div class="col-md-3 pt-4">
+                <a class="mr-5" style="color:white;" href="">Ana Sayfa</a>
+                <a class="mr-5" style="color:white;" href="">Hakkımızda</a>
+                <a style="color:white;" href="">İletişim</a>
+            </div>
+            <div class="col-md-4 pt-3">
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+            <div class="col-md-3">
+                
+            </div>
+        </div>
+    </div>
+</div>
